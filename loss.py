dbf55834114994b1e7c3d0e5e18d98c4353d4290
Siboooo <sibo.zhao1996@hotmail.com>
2018-11-24 19:56:42 +0000

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,1 +1,9 @@
 import tensorflow as tf
+
+IMAGE_SHAPE = (400, 400, 3)
+
+def PSNR(yTar, yRes):
+    return tf.image.psnr(yTar, yRes, max_val=1.0)
+
+def SSIM(yTar, yRes):
+    return tf.image.ssim(yTar, yRes, max_val=1.0)
