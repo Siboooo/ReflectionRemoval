10d9ac8e234ff201c122a3f87b2c431466d2e926
Siboooo <sibo.zhao1996@hotmail.com>
2018-11-25 14:36:36 +0000

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,9 +1,15 @@
 import tensorflow as tf
 
-IMAGE_SHAPE = (400, 400, 3)
+IMAGE_SHAPE = (400, 540, 3)
 
 def PSNR(yTar, yRes):
     return tf.image.psnr(yTar, yRes, max_val=1.0)
 
 def SSIM(yTar, yRes):
     return tf.image.ssim(yTar, yRes, max_val=1.0)
+
+def l1_loss(yTar, yRes):
+    return tf.reduce_mean(tf.abs(yRes - yTar))
+
+def wasserstein_loss(yTar, yRes):
+    return tf.reduce_mean(tf.multiply(yTar, yRes))
