8efaec08b260690cd6b5cfdaa2ab5704e5b51244
Siboooo <sibo.zhao1996@hotmail.com>
2018-12-07 09:28:29 +0000

    Update loss.py

--- loss.py
+++ loss.py
@@ -14,13 +14,10 @@ def l1_loss(yTar, yRes):
     return tf.reduce_mean(tf.abs(yRes - yTar))
 
 def perceptual_loss(yTar, yRes):
-    #vgg = keras.applications.vgg16(include_top=False, weights='imagenet', input_shape=IMAGE_SHAPE)
-    #model = keras.models(inputs = vgg.input, outputs = vgg.get_layer('block3_conv3').output)
     vgg = VGG16(include_top=False, weights='imagenet', input_shape=IMAGE_SHAPE)
     loss_model = Model(inputs=vgg.input, outputs=vgg.get_layer('block3_conv3').output)
     loss_model.trainable = False
     return tf.reduce_mean(tf.square(loss_model(yTar) - loss_model(yRes)))
 
 def wasserstein_loss(yTar, yRes):
-    #return tf.reduce_mean(tf.multiply(yTar, yRes))
     return tf.abs(tf.reduce_mean(tf.multiply(yTar, yRes)))
